eed2f4c335530ab193965ebfc71d19ea69fbc99f
ledet20 <ledet2016@gmail.com>
2018-08-05 11:38:48 -0500

    header component complete

--- gitsave/src/App.js
+++ gitsave/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Header } from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <p> testing </p>
-      </div>
+     <Header />
     );
   }
 }
---
+++ gitsave/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../stylesheets/header.css';
+
+export const Header = () => (
+    <div>
+        <h1 className='title'> GITSAVE </h1>
+        <p className='logo'> open source contributions made easy </p>
+    </div>
+);
